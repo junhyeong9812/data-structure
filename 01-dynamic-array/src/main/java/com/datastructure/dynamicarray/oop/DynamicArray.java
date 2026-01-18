9fb50d6db90696e78e0bdac8df188ebd174a2bd4
junhyeong9812 <pickjog@gmail.com>
2026-01-18 18:21:53 +0900

    feat: oop 배열 인터페이스 추가

--- 01-dynamic-array/src/main/java/com/datastructure/dynamicarray/oop/DynamicArray.java
+++ 01-dynamic-array/src/main/java/com/datastructure/dynamicarray/oop/DynamicArray.java
@@ -1,4 +1,24 @@
 package com.datastructure.dynamicarray.oop;
 
 public interface DynamicArray<E> {
+
+    public void add(E element);
+
+    public void add(int index, E element);
+
+    public E get(int index);
+
+    public E set(int index, E element);
+
+    public E remove(int index);
+
+    public int size();
+
+    public boolean isEmpty();
+
+    public boolean contains(E element);
+
+    public int indexOf(E element);
+
+    public void clear();
 }
