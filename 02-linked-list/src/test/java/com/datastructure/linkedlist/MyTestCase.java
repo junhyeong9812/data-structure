acb79cd4f20f174e612b895762f61316e2223be6
junhyeong9812 <pickjog@gmail.com>
2026-01-20 19:03:15 +0900

    feat: 테스트 케이스 포맷작성

---
+++ 02-linked-list/src/test/java/com/datastructure/linkedlist/MyTestCase.java
@@ -0,0 +1,185 @@
+package com.datastructure.linkedlist;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class MyTestCase {
+
+    @Nested
+    @DisplayName("단일 연결 리스트")
+    class SinglyLinkedListTest {
+
+        @Nested
+        @DisplayName("addFirst 메서드 테스트")
+        class AddFirstTest {
+
+        }
+
+        @Nested
+        @DisplayName("addLast 메서드 테스트")
+        class AddLastTest {
+
+        }
+
+        @Nested
+        @DisplayName("add 메서드 테스트")
+        class AddTest {
+
+        }
+
+        @Nested
+        @DisplayName("removeFirst 메서드 테스트")
+        class RemoveFirstTest {
+
+        }
+
+        @Nested
+        @DisplayName("removeLast 메서드 테스트")
+        class RemoveLastTest {
+
+        }
+
+        @Nested
+        @DisplayName("remove 메서드 테스트")
+        class RemoveTest {
+
+        }
+
+        @Nested
+        @DisplayName("get 메서드 테스트")
+        class GetTest {
+
+        }
+
+        @Nested
+        @DisplayName("set 메서드 테스트")
+        class SetTest {
+
+        }
+
+        @Nested
+        @DisplayName("size 메서드 테스트")
+        class SizeTest {
+
+        }
+
+        @Nested
+        @DisplayName("isEmpty 메서드 테스트")
+        class IsEmptyTest {
+
+        }
+
+        @Nested
+        @DisplayName("contains 메서드 테스트")
+        class ContainsTest {
+
+        }
+
+        @Nested
+        @DisplayName("indexOf 메서드 테스트")
+        class IndexOfTest {
+
+        }
+
+        @Nested
+        @DisplayName("clear 메서드 테스트")
+        class ClearTest {
+
+        }
+
+        @Nested
+        @DisplayName("reverse 메서드 테스트")
+        class ReverseTest {
+
+        }
+    }
+
+    @Nested
+    @DisplayName("이중 연결 리스트")
+    class DoublyLinkedListTest {
+        @Nested
+        @DisplayName("addFirst 메서드 테스트")
+        class AddFirstTest {
+
+        }
+
+        @Nested
+        @DisplayName("addLast 메서드 테스트")
+        class AddLastTest {
+
+        }
+
+        @Nested
+        @DisplayName("add 메서드 테스트")
+        class AddTest {
+
+        }
+
+        @Nested
+        @DisplayName("removeFirst 메서드 테스트")
+        class RemoveFirstTest {
+
+        }
+
+        @Nested
+        @DisplayName("removeLast 메서드 테스트")
+        class RemoveLastTest {
+
+        }
+
+        @Nested
+        @DisplayName("remove 메서드 테스트")
+        class RemoveTest {
+
+        }
+
+        @Nested
+        @DisplayName("get 메서드 테스트")
+        class GetTest {
+
+        }
+
+        @Nested
+        @DisplayName("set 메서드 테스트")
+        class SetTest {
+
+        }
+
+        @Nested
+        @DisplayName("size 메서드 테스트")
+        class SizeTest {
+
+        }
+
+        @Nested
+        @DisplayName("isEmpty 메서드 테스트")
+        class IsEmptyTest {
+
+        }
+
+        @Nested
+        @DisplayName("contains 메서드 테스트")
+        class ContainsTest {
+
+        }
+
+        @Nested
+        @DisplayName("indexOf 메서드 테스트")
+        class IndexOfTest {
+
+        }
+
+        @Nested
+        @DisplayName("clear 메서드 테스트")
+        class ClearTest {
+
+        }
+
+        @Nested
+        @DisplayName("reverse 메서드 테스트")
+        class ReverseTest {
+
+        }
+    }
+}
