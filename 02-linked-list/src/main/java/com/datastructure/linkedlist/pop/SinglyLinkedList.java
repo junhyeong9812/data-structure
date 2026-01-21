fb3c753a34c65a064b690aac36612e63e73b4422
junhyeong9812 <pickjog@gmail.com>
2026-01-21 12:19:10 +0900

    feat: 메서드 시그니처 완료

--- 02-linked-list/src/main/java/com/datastructure/linkedlist/pop/SinglyLinkedList.java
+++ 02-linked-list/src/main/java/com/datastructure/linkedlist/pop/SinglyLinkedList.java
@@ -1,4 +1,56 @@
 package com.datastructure.linkedlist.pop;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList<E> {
+
+    public void addFirst(E element) {
+        
+    }
+
+    public void addLast(E element) {
+
+    }
+
+    public void add(int index, E element) {
+
+    }
+
+    public E removeFirst() {
+        return null;
+    }
+
+    public E removeLast() {
+        return null;
+    }
+
+    public E remove(int index) {
+        return null;
+    }
+
+    public E get(int index) {
+        return null;
+    }
+
+    public E set(int index, E element) {
+        return null;
+    }
+
+    public int size() {
+        return  0;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public boolean contains(E element) {
+        return true;
+    }
+
+    public int indexOf(E element) {
+        return -1;
+    }
+
+    public void clear() {}
+
+    public void reverse() {}
 }
