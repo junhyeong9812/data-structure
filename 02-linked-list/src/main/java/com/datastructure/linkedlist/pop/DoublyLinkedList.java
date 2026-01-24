2d681642611b95f7df27a14eee956e4966d68a7a
junhyeong9812 <pickjog@gmail.com>
2026-01-24 21:30:42 +0900

    feat: addFirst 구현

--- 02-linked-list/src/main/java/com/datastructure/linkedlist/pop/DoublyLinkedList.java
+++ 02-linked-list/src/main/java/com/datastructure/linkedlist/pop/DoublyLinkedList.java
@@ -1,4 +1,85 @@
 package com.datastructure.linkedlist.pop;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList<E> {
+
+    private Node<E> head = null;
+    private Node<E> tail = null;
+    private int size = 0;
+
+    public static class Node<E> {
+        E data;
+        Node<E> prev;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+
+        Node(E data, Node<E> prev, Node<E> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            head.prev = newNode;
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(E element) {
+
+    }
+
+    public void add(int index, E element) {
+
+    }
+
+    public E removeFirst() {
+        return null;
+    }
+
+    public E removeLast() {
+        return null;
+    }
+
+    public E remove(int index) {
+        return null;
+    }
+
+    public E get(int index) {
+        return null;
+    }
+
+    public E set(int index, E element) {
+        return null;
+    }
+
+    public int size() {
+        return  0;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public boolean contains(E element) {
+        return true;
+    }
+
+    public int indexOf(E element) {
+        return -1;
+    }
+
+    public void clear() {}
+
+    public void reverse() {}
 }
