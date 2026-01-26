d781468f14b221f747218cd448e8ef126fb00634
junhyeong9812 <pickjog@gmail.com>
2026-01-26 10:25:21 +0900

    feat: addFirst 인터페이스 정의 완료

--- 02-linked-list/src/main/java/com/datastructure/linkedlist/oop/SinglyLinkedListImpl.java
+++ 02-linked-list/src/main/java/com/datastructure/linkedlist/oop/SinglyLinkedListImpl.java
@@ -1,4 +1,86 @@
 package com.datastructure.linkedlist.oop;
 
 public class SinglyLinkedListImpl<E> implements LinkedList<E> {
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    private static class Node<E> {
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+
+        Node(E data, Node<E> next) {
+            this.data = data;
+            this.next = next;
+        }
+    }
+
+    public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(E element) {
+
+    }
+
+    public void add(int index, E element) {
+
+    }
+
+    public E removeFirst() {
+
+    }
+
+    public E removeLast() {
+
+    }
+
+    public E remove() {
+
+    }
+
+    public E get(int index) {
+
+    }
+
+    public E set(int index, E element) {
+
+    }
+
+    public int size() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public boolean contains(E element) {
+
+    }
+
+    public int indexOf(E element) {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public void reverse() {
+
+    }
 }
