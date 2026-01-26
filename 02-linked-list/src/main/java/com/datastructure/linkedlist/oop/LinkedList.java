6818dbecb58ec1400c393e35ba30efe7108a6c3d
junhyeong9812 <pickjog@gmail.com>
2026-01-26 09:39:30 +0900

    feat: LinkedList 인터페이스 정의 완료

--- 02-linked-list/src/main/java/com/datastructure/linkedlist/oop/LinkedList.java
+++ 02-linked-list/src/main/java/com/datastructure/linkedlist/oop/LinkedList.java
@@ -1,4 +1,32 @@
 package com.datastructure.linkedlist.oop;
 
 public interface LinkedList<E> {
+
+    public void addFirst(E element);
+
+    public void addLast(E element);
+
+    public void add(int index, E element);
+
+    public E removeFirst();
+
+    public E removeLast();
+
+    public E remove();
+
+    public E get(int index);
+
+    public E set(int index, E element);
+
+    public int size();
+
+    public boolean isEmpty();
+
+    public boolean contains(E element);
+
+    public int indexOf(E element);
+
+    public void clear();
+
+    public void reverse();
 }
