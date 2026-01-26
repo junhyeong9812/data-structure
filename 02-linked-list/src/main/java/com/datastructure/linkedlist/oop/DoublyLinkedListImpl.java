74702b979a37d3d06ad27d9adcd0b8ecfc60c16d
junhyeong9812 <pickjog@gmail.com>
2026-01-26 16:28:52 +0900

    feat: add 계열 메서드 추가 및 초기 객체 및 변수 추가

--- 02-linked-list/src/main/java/com/datastructure/linkedlist/oop/DoublyLinkedListImpl.java
+++ 02-linked-list/src/main/java/com/datastructure/linkedlist/oop/DoublyLinkedListImpl.java
@@ -1,4 +1,118 @@
 package com.datastructure.linkedlist.oop;
 
 public class DoublyLinkedListImpl<E> implements LinkedList<E> {
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+
+    private static class Node<E> {
+        E data;
+        Node<E> prev;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+
+        Node(E data, Node<E> prev, Node<E> next) {
+            this.data = data;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+
+    public void addFirst(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        }
+        size++;
+    }
+
+    public void addLast(E element) {
+        Node<E> newNode = new Node<>(element);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.prev = tail;
+            tail.next = newNode;
+            tail = newNode;
+        }
+        size++;
+    }
+
+    public void add(int index, E element) {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0) {
+            addFirst(element);
+            return;
+        }
+        if (index == size) {
+            addLast(element);
+            return;
+        }
+        Node<E> beforeNode = head;
+        for (int i = 0; i < index - 1; i++) {
+            beforeNode = beforeNode.next;
+        }
+        Node<E> newNode = new Node<>(element);
+        newNode.next = beforeNode.next;
+        newNode.prev = beforeNode;
+        beforeNode.next = newNode;
+        newNode.next.prev = newNode;
+        size++;
+    }
+
+    public E removeFirst() {
+
+    }
+
+    public E removeLast() {
+
+    }
+
+    public E remove(int index) {
+
+    }
+
+    public E get(int index) {
+
+    }
+
+    public E set(int index, E element) {
+
+    }
+
+    public int size() {
+
+    }
+
+    public boolean isEmpty() {
+
+    }
+
+    public boolean contains(E element) {
+
+    }
+
+    public int indexOf(E element) {
+
+    }
+
+    public void clear() {
+
+    }
+
+    public void reverse() {
+
+    }
 }
