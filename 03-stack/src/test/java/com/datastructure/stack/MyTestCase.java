37e00043022a8f309d8aa445b0d104e53f366a33
junhyeong9812 <pickjog@gmail.com>
2026-01-27 18:36:07 +0900

    feat: 연결리스트 기반 스택 테스트 분리

--- 03-stack/src/test/java/com/datastructure/stack/MyTestCase.java
+++ 03-stack/src/test/java/com/datastructure/stack/MyTestCase.java
@@ -1,6 +1,7 @@
 package com.datastructure.stack;
 
 import com.datastructure.stack.pop.ArrayStack;
+import com.datastructure.stack.pop.LinkedStack;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -71,6 +72,59 @@ public class MyTestCase {
     @DisplayName("연결 리스트 기반 스택")
     class LinkedStackTest {
 
+        LinkedStack stack ;
+        @BeforeEach
+        void setup() {
+            stack = new LinkedStack();
+        }
+
+        @Nested
+        @DisplayName("연결 리스트 기반 스택 생성 테스트")
+        class Creation {
+
+        }
+
+        @Nested
+        @DisplayName("push 메서드 테스트")
+        class PushTest {
+
+        }
+
+        @Nested
+        @DisplayName("pop 메서드 테스트")
+        class PopTest {
+
+        }
+
+        @Nested
+        @DisplayName("peek 메서드 테스트")
+        class peekTest {
+
+        }
+
+        @Nested
+        @DisplayName("top 메서드 테스트")
+        class TopTest {
+
+        }
+
+        @Nested
+        @DisplayName("isEmpty 메서드 테스트")
+        class IsEmptyTest {
+
+        }
+
+        @Nested
+        @DisplayName("size 메서드 테스트")
+        class SizeTest {
+
+        }
+
+        @Nested
+        @DisplayName("clear 메서드 테스트")
+        class ClearTest {
+
+        }
     }
 
 
