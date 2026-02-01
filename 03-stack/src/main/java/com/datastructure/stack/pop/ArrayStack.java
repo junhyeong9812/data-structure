5bb70acfc451bee5c0e562d1b6db83c0d4f61825
junhyeong9812 <pickjog@gmail.com>
2026-02-02 07:33:35 +0900

    feat: 객체 메서드 시그니처 추가

--- 03-stack/src/main/java/com/datastructure/stack/pop/ArrayStack.java
+++ 03-stack/src/main/java/com/datastructure/stack/pop/ArrayStack.java
@@ -1,4 +1,52 @@
 package com.datastructure.stack.pop;
 
-public class ArrayStack {
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayStack<E> {
+    private int capacity = 10;
+    private E[] data;
+    private int top;
+
+    @SuppressWarnings("unChecked")
+    public ArrayStack() {
+        this.data = (E[])new Object[capacity];
+        top = 0;
+    }
+
+    public void push(E element) {
+
+    }
+
+    public E pop() {
+        return null;
+    }
+
+    public E peek() {
+        return null;
+    }
+
+    public E top() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public int search(E element) {
+        return 0;
+    }
+
+    public List<E> toArray() {
+        return null;
+    }
 }
