48a78bc633327ccb099092efb428bbb06ccf2ed8
junhyeong9812 <pickjog@gmail.com>
2026-02-02 07:36:51 +0900

    feat: 연결 스택 시그니쳐 메서드 추가

--- 03-stack/src/main/java/com/datastructure/stack/pop/LinkedStack.java
+++ 03-stack/src/main/java/com/datastructure/stack/pop/LinkedStack.java
@@ -1,4 +1,49 @@
 package com.datastructure.stack.pop;
 
-public class LinkedStack {
+import java.util.List;
+
+public class LinkedStack<E> {
+
+    private Node<E> top;
+
+    public static class Node<E> {
+        E data;
+        Node<E> next;
+    }
+
+    public void push(E element) {
+
+    }
+
+    public E pop() {
+        return null;
+    }
+
+    public E peek() {
+        return null;
+    }
+
+    public E top() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+
+    public void clear() {
+
+    }
+
+    public int search(E element) {
+        return 0;
+    }
+
+    public List<E> toArray() {
+        return null;
+    }
 }
