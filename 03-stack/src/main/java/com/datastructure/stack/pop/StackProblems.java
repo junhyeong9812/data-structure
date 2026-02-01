082c85e10dfdc038d036829d33c99dd8cb4c3bb6
junhyeong9812 <pickjog@gmail.com>
2026-02-02 07:39:46 +0900

    feat: 응용 문제 메서드 시그니처 추가

--- 03-stack/src/main/java/com/datastructure/stack/pop/StackProblems.java
+++ 03-stack/src/main/java/com/datastructure/stack/pop/StackProblems.java
@@ -1,4 +1,15 @@
 package com.datastructure.stack.pop;
 
 public class StackProblems {
+    public boolean isValidParentheses(String data) {
+        return false;
+    }
+
+    public int evaluatePostfix(String data) {
+        return 0;
+    }
+
+    public int infixToPostfix(String data) {
+        return 0;
+    }
 }
