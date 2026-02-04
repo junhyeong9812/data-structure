4d4a66f65724f4526b68acac4f937f71812932fd
junhyeong9812 <pickjog@gmail.com>
2026-02-05 07:32:32 +0900

    feat: isValidParentheses 1차 실패

--- 03-stack/src/main/java/com/datastructure/stack/pop/StackProblems.java
+++ 03-stack/src/main/java/com/datastructure/stack/pop/StackProblems.java
@@ -2,7 +2,86 @@ package com.datastructure.stack.pop;
 
 public class StackProblems {
     public boolean isValidParentheses(String data) {
-        return false;
+        char[] datas = data.toCharArray();
+        Parentheses parentheses = new Parentheses();
+        Braces braces = new Braces();
+        Brackets brackets = new Brackets();
+
+        for(int i = 0; i <datas.length; i++) {
+            parentheses.checkParentheses(datas[i]);
+            braces.checkBraces(datas[i]);
+            brackets.checkBrackets(datas[i]);
+        }
+
+        return parentheses.isMatched() && braces.isMatched() && brackets.isMatched();
+    }
+
+    private class Parentheses {
+        private int openParenthesis;
+        private int closeParenthesis;
+
+        public Parentheses() {
+            this.openParenthesis = 0;
+            this.closeParenthesis = 0;
+        }
+
+        public void checkParentheses(char data) {
+            if (data == '(') {
+                openParenthesis++;
+            }
+            if (data == ')') {
+                closeParenthesis++;
+            }
+        }
+        public boolean isMatched() {
+            return openParenthesis == closeParenthesis;
+        }
+    }
+
+    private class Braces {
+        private int openBrace;
+        private int closeBrace;
+
+        public Braces() {
+            this.openBrace = 0;
+            this.closeBrace = 0;
+        }
+
+        public void checkBraces(char data) {
+            if (data == '{') {
+                openBrace++;
+            }
+            if (data == '}') {
+                closeBrace++;
+            }
+        }
+
+        public boolean isMatched() {
+            return openBrace == closeBrace;
+        }
+    }
+
+    private class Brackets {
+        private int openBracket;
+        private int closeBracket;
+
+        public Brackets() {
+            this.openBracket = 0;
+            this.closeBracket = 0;
+        }
+
+        public void checkBrackets(char data) {
+            if (data == '[') {
+                openBracket++;
+            }
+            if (data == ']') {
+                closeBracket++;
+            }
+        }
+
+        public boolean isMatched() {
+            return openBracket == closeBracket;
+        }
     }
 
     public int evaluatePostfix(String data) {
