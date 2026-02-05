9d4f0b599941e4d2360a33eb1a42bed3e48e9bba
junhyeong9812 <pickjog@gmail.com>
2026-02-05 09:00:31 +0900

    feat: evalutePostfix 진행중

--- 03-stack/src/main/java/com/datastructure/stack/pop/StackProblems.java
+++ 03-stack/src/main/java/com/datastructure/stack/pop/StackProblems.java
@@ -1,63 +1,103 @@
 package com.datastructure.stack.pop;
 
 
+import java.util.Objects;
+
 public class StackProblems {
     public boolean isValidParentheses(String data) {
         char[] datas = data.toCharArray();
 
         // 데이터를 스택에 저장
 
-        ValidateParentheses validateParentheses = new ValidateParentheses();
+        int size = datas.length;
+        ArrayStack<Character> openStack = new ArrayStack<>();
+        for (int i = 0; i < size; i++) {
+            if (datas[i] == '(') {
+                openStack.push(datas[i]);
 
-        return validateParentheses.isValid(datas);
-    }
+            }
+            if (datas[i] == '{') {
+                openStack.push(datas[i]);
 
-    public class ValidateParentheses {
+            }
+            if (datas[i] == '[') {
+                openStack.push(datas[i]);
 
-        public boolean isValid(char[] datas) {
-            int size = datas.length;
-            ArrayStack<Character> openStack = new ArrayStack<>();
-            for (int i = 0; i < size; i++) {
-                char data = datas[i];
-                if (data == '(') {
-                    openStack.push(data);
-
-                }
-                if (data == '{') {
-                    openStack.push(data);
-
-                }
-                if (data == '[') {
-                    openStack.push(data);
-
-                }
-                if (data == ')') {
-                    if (openStack.isEmpty() || openStack.pop() != '(') {
-                        return false;
-                    }
-                }
-                if (data == '}') {
-                    if (openStack.isEmpty() || openStack.pop() != '{') {
-                        return false;
-                    }
-                }
-                if (data == ']') {
-                    if (openStack.isEmpty() || openStack.pop() != '[') {
-                        return false;
-                    }
+            }
+            if (datas[i] == ')') {
+                if (openStack.isEmpty() || openStack.pop() != '(') {
+                    return false;
                 }
             }
-            if (!openStack.isEmpty()) {
-                return false;
+            if (datas[i] == '}') {
+                if (openStack.isEmpty() || openStack.pop() != '{') {
+                    return false;
+                }
             }
-            return true;
+            if (datas[i] == ']') {
+                if (openStack.isEmpty() || openStack.pop() != '[') {
+                    return false;
+                }
+            }
         }
+        if (!openStack.isEmpty()) {
+            return false;
+        }
+        return true;
     }
+
 
 
 
     public int evaluatePostfix(String data) {
-        return 0;
+        if (data.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        String[] tokens = data.split(" ");
+
+        ArrayStack<Integer> numbers = new ArrayStack<>();
+        for (int i = 0; i < tokens.length; i++) {
+            if (isNumber(String.valueOf(tokens[i]))) {
+                numbers.push(Integer.parseInt(tokens[i]));
+            } else {
+                if (Objects.equals(tokens[i],"+")) {
+                    int rightOperand = numbers.pop();
+                    int leftOperand = numbers.pop();
+                    numbers.push(leftOperand + rightOperand);
+                }
+                if (Objects.equals(tokens[i],"-")) {
+                    int rightOperand = numbers.pop();
+                    int leftOperand = numbers.pop();
+                    numbers.push(leftOperand - rightOperand);
+                }
+                if (Objects.equals(tokens[i],"*")) {
+                    int rightOperand = numbers.pop();
+                    int leftOperand = numbers.pop();
+                    numbers.push(leftOperand * rightOperand);
+                }
+                if (Objects.equals(tokens[i],"/")) {
+                    int rightOperand = numbers.pop();
+                    int leftOperand = numbers.pop();
+                    numbers.push(leftOperand / rightOperand);
+                }
+                if (Objects.equals(tokens[i],"%")) {
+                    int rightOperand = numbers.pop();
+                    int leftOperand = numbers.pop();
+                    numbers.push(leftOperand % rightOperand);
+                }
+            }
+
+        }
+        return numbers.pop();
+    }
+
+    public boolean isNumber(String data) {
+        try {
+            Integer.parseInt(data);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 
     public String infixToPostfix(String data) {
