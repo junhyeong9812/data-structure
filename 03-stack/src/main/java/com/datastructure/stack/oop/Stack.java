c53b2b71cedb3d5b014d3ea0156d5a97531a338b
junhyeong9812 <pickjog@gmail.com>
2026-02-08 07:27:08 +0900

    feat: oop stack Interface 추가

--- 03-stack/src/main/java/com/datastructure/stack/oop/Stack.java
+++ 03-stack/src/main/java/com/datastructure/stack/oop/Stack.java
@@ -1,4 +1,18 @@
 package com.datastructure.stack.oop;
 
 public interface Stack<E> {
+
+    public void push(E element);
+
+    public E pop();
+
+    public E peek();
+
+    public E top();
+
+    public boolean isEmpty();
+
+    public int size();
+
+    public void clear();
 }
