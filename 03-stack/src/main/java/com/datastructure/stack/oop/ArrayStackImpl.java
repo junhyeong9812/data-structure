167578552eabc4c777865e7272717a37cd6d52fa
junhyeong9812 <pickjog@gmail.com>
2026-02-08 07:28:24 +0900

    feat: 인터페이스 메서드 시그니처 추가

--- 03-stack/src/main/java/com/datastructure/stack/oop/ArrayStackImpl.java
+++ 03-stack/src/main/java/com/datastructure/stack/oop/ArrayStackImpl.java
@@ -1,4 +1,33 @@
 package com.datastructure.stack.oop;
 
 public class ArrayStackImpl<E> implements Stack<E> {
+
+    public void push(E element) {
+
+    }
+
+    public E pop() {
+        return null;
+    }
+
+    public E peek() {
+        return null;
+    }
+
+    public E top() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+
+        return true;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+    public void clear() {
+
+    }
 }
