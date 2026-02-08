18867c8263e7a511d4cc6332cf667af0617e4cff
junhyeong9812 <pickjog@gmail.com>
2026-02-09 08:30:18 +0900

    feat: 스택 문제 구현체 추가

---
+++ 03-stack/src/main/java/com/datastructure/stack/oop/StackProblemsImpl.java
@@ -0,0 +1,26 @@
+package com.datastructure.stack.oop;
+
+public class StackProblemsImpl implements StackProblems{
+    private ParenthesesValidator validator;
+    private StackCalculator calculator;
+
+    public StackProblemsImpl() {
+        this.validator = new ParenthesesValidator();
+        this.calculator = new StackCalculator();
+    }
+
+    @Override
+    public boolean isValidParentheses(String data) {
+        return false;
+    }
+
+    @Override
+    public int evaluatePostfix(String data) {
+        return 0;
+    }
+
+    @Override
+    public String infixToPostfix(String data) {
+        return "";
+    }
+}
