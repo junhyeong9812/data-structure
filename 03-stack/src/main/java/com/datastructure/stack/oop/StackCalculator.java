a54873314c82c69e2c2993349c74479c0b5d5ff6
junhyeong9812 <pickjog@gmail.com>
2026-02-10 07:42:36 +0900

    feat: stack 계산기 시그니쳐 추가

--- 03-stack/src/main/java/com/datastructure/stack/oop/StackCalculator.java
+++ 03-stack/src/main/java/com/datastructure/stack/oop/StackCalculator.java
@@ -1,4 +1,10 @@
 package com.datastructure.stack.oop;
 
 public class StackCalculator {
+    public int evaluatePostfix(String data) {
+        return 0;
+    }
+    public String infixToPostfix(String data){
+        return null;
+    }
 }
