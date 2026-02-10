721485f53de1f29b4d3d4b8fff5f5c84be6eb9ce
junhyeong9812 <pickjog@gmail.com>
2026-02-11 07:28:32 +0900

    feat: 후위 계싼 문제 완료

--- 03-stack/src/main/java/com/datastructure/stack/oop/StackCalculator.java
+++ 03-stack/src/main/java/com/datastructure/stack/oop/StackCalculator.java
@@ -1,7 +1,73 @@
 package com.datastructure.stack.oop;
 
 public class StackCalculator {
+//    public int evaluatePostfix(String data) {
+//        String[] datas = data.split(" ");
+//
+//        Stack<Integer> numbers = new ArrayStackImpl<>();
+//        for (int i = 0; i < datas.length; i++) {
+//            if (isNumber(datas[i])) {
+//                numbers.push(Integer.parseInt(String.valueOf(datas[i])));
+//            }
+//            if (datas[i].equals("+") ) {
+//                if (numbers.size() >= 2) {
+//                    int rightNumber= numbers.pop();
+//                    int leftNumber= numbers.pop();
+//                    numbers.push(leftNumber + rightNumber);
+//                } else {
+//                    throw new IllegalArgumentException();
+//                }
+//            }
+//            if (datas[i].equals("-")) {
+//                if (numbers.size() >= 2) {
+//                    int rightNumber= numbers.pop();
+//                    int leftNumber= numbers.pop();
+//                    numbers.push(leftNumber - rightNumber);
+//                } else {
+//                    throw new IllegalArgumentException();
+//                }
+//            }
+//            if (datas[i].equals("*")) {
+//                if (numbers.size() >= 2) {
+//                    int rightNumber= numbers.pop();
+//                    int leftNumber= numbers.pop();
+//                    numbers.push(leftNumber * rightNumber);
+//                } else {
+//                    throw new IllegalArgumentException();
+//                }
+//            }
+//            if (datas[i].equals("/")) {
+//                if (numbers.size() >= 2) {
+//                    int rightNumber= numbers.pop();
+//                    int leftNumber= numbers.pop();
+//                    if (rightNumber == 0) {
+//                        throw new ArithmeticException();
+//                    }
+//                    numbers.push(leftNumber / rightNumber);
+//                } else {
+//                    throw new IllegalArgumentException();
+//                }
+//            }
+//            if (datas[i].equals("%")) {
+//                if (numbers.size() >= 2) {
+//                    int rightNumber= numbers.pop();
+//                    int leftNumber= numbers.pop();
+//                    if (rightNumber == 0) {
+//                        throw new ArithmeticException();
+//                    }
+//                    numbers.push(leftNumber % rightNumber);
+//                } else {
+//                    throw new IllegalArgumentException();
+//                }
+//            }
+//        }
+//        return numbers.pop();
+//    }
+
     public int evaluatePostfix(String data) {
+
+        if (data == null || data.trim().isEmpty()) throw new IllegalArgumentException();
+
         String[] datas = data.split(" ");
 
         Stack<Integer> numbers = new ArrayStackImpl<>();
@@ -10,62 +76,46 @@ public class StackCalculator {
                 numbers.push(Integer.parseInt(String.valueOf(datas[i])));
             }
             if (datas[i].equals("+") ) {
-                if (numbers.size() >= 2) {
-                    int rightNumber= numbers.pop();
-                    int leftNumber= numbers.pop();
-                    numbers.push(leftNumber + rightNumber);
-                } else {
-                    throw new IllegalArgumentException();
-                }
+                calculate(numbers,"+");
             }
             if (datas[i].equals("-")) {
-                if (numbers.size() >= 2) {
-                    int rightNumber= numbers.pop();
-                    int leftNumber= numbers.pop();
-                    numbers.push(leftNumber - rightNumber);
-                } else {
-                    throw new IllegalArgumentException();
-                }
+                calculate(numbers,"-");
             }
             if (datas[i].equals("*")) {
-                if (numbers.size() >= 2) {
-                    int rightNumber= numbers.pop();
-                    int leftNumber= numbers.pop();
-                    numbers.push(leftNumber * rightNumber);
-                } else {
-                    throw new IllegalArgumentException();
-                }
+                calculate(numbers,"*");
             }
             if (datas[i].equals("/")) {
-                if (numbers.size() >= 2) {
-                    int rightNumber= numbers.pop();
-                    int leftNumber= numbers.pop();
-                    if (rightNumber == 0) {
-                        throw new ArithmeticException();
-                    }
-                    numbers.push(leftNumber / rightNumber);
-                } else {
-                    throw new IllegalArgumentException();
-                }
+                calculate(numbers,"/");
             }
             if (datas[i].equals("%")) {
-                if (numbers.size() >= 2) {
-                    int rightNumber= numbers.pop();
-                    int leftNumber= numbers.pop();
-                    if (rightNumber == 0) {
-                        throw new ArithmeticException();
-                    }
-                    numbers.push(leftNumber % rightNumber);
-                } else {
-                    throw new IllegalArgumentException();
-                }
+                calculate(numbers,"%");
             }
         }
         return numbers.pop();
     }
 
-    private void calculate(Stack<Integer> stack, String operator) {
-        int
+    private void calculate(Stack<Integer> numbers, String operator) {
+        int[] operands = popOperand(numbers);
+
+        if ((operator.equals("/") || operator.equals("%")) && operands[1] == 0) {
+            throw new ArithmeticException();
+        }
+
+        int result = switch (operator) {
+            case "+" -> operands[0] + operands[1];
+            case "-" -> operands[0] - operands[1];
+            case "*" -> operands[0] * operands[1];
+            case "/" -> operands[0] / operands[1];
+            default -> operands[0] % operands[1];
+        };
+        numbers.push(result);
+    }
+
+    private int[] popOperand(Stack<Integer> numbers) {
+        if (numbers.size() < 2) throw new IllegalArgumentException();
+        int right = numbers.pop();
+        int left = numbers.pop();
+        return new int[] {left, right};
     }
 
     private boolean isNumber(String data) {
