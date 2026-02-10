8f66722017890161e2bc5512543b4110761962c3
junhyeong9812 <pickjog@gmail.com>
2026-02-11 08:56:50 +0900

    test: 대분류 시그니처 추가

---
+++ 04-queue-deque/src/test/java/com/datastructure/queue/MyTestCase.java
@@ -0,0 +1,44 @@
+package com.datastructure.queue;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+public class MyTestCase {
+
+    @Nested
+    @DisplayName("배열 기반 큐")
+    class ArrayQueueTest {}
+
+    @Nested
+    @DisplayName("연결 리스트 기반 큐")
+    class LinkedListQueueTest {
+
+    }
+
+    @Nested
+    @DisplayName("원형 큐")
+    class CircularQueueTest {
+
+    }
+
+    @Nested
+    @DisplayName("배열 기반 덱")
+    class ArrayDequeTest {
+
+    }
+
+    @Nested
+    @DisplayName("연결 리스트 기반 덱")
+    class LinkedListDequeTest {
+
+    }
+
+
+    @Nested
+    @DisplayName("추가 응용 테스트")
+    class ApplicationTest {
+
+    }
+
+}
