6ef4aa720b2e6f0e91a2bd80111edb3c735055f3
junhyeong9812 <pickjog@gmail.com>
2026-02-17 10:20:59 +0900

    feat: CircularQueue 초기값 작성

--- 04-queue-deque/src/main/java/com/datastructure/queue/pop/CircularQueue.java
+++ 04-queue-deque/src/main/java/com/datastructure/queue/pop/CircularQueue.java
@@ -1,4 +1,27 @@
 package com.datastructure.queue.pop;
 
-public class CircularQueue {
+public class CircularQueue<E> {
+    private int capacity;
+    private E[] elements;
+    private int front;
+    private int rear;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public CircularQueue() {
+        this.capacity = 10;
+        this.elements = (E[])(new Object[this.capacity]);
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public CircularQueue(int capacity) {
+        this.capacity = capacity;
+        this.elements = (E[])(new Object[this.capacity]);
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
+    }
 }
