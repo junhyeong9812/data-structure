b4e12e9c6ea3f1e59e06a6a0ef48e964e6c76b45
junhyeong9812 <pickjog@gmail.com>
2026-02-22 17:44:43 +0900

    feat: 연습용 다이나믹 인터페이스 구현

---
+++ practice/src/main/java/com/datastructure/practice0222/array/DynamicArray.java
@@ -0,0 +1,24 @@
+package com.datastructure.practice0222.array;
+
+public interface DynamicArray<E> extends Iterable<E> {
+
+    public void add(E element);
+
+    public void add(int index, E element);
+
+    public E get(int index);
+
+    public E set(int index, E element);
+
+    public E remove(int index);
+
+    public int size();
+
+    public boolean isEmpty();
+
+    public boolean contains(E element);
+
+    public int indexOf(E element);
+
+    public void clear();
+}
