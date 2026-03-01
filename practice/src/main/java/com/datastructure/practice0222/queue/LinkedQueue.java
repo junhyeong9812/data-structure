b333cfda64b7de89349eda147802adbf2d792f21
junhyeong9812 <pickjog@gmail.com>
2026-03-01 18:48:07 +0900

    feat: linkedQueue 초기 생성

---
+++ practice/src/main/java/com/datastructure/practice0222/queue/LinkedQueue.java
@@ -0,0 +1,21 @@
+package com.datastructure.practice0222.queue;
+
+/**
+ * 연결 리스트 기반 큐 (inked Queue)
+ *
+ * head에서 dequeue, tail에서 enqueue.
+ * 모든 연산 0(1), 용량 제한 없음.
+ */
+public class LinkedQueue<E> implements Queue<E> {
+
+    private static class Node<E> {
+        E value;
+        Node<E> next;
+
+        Node(E value) {this.value = value; }
+    }
+
+    private Node<E> head;
+    private Node<E> tail;
+    private int size;
+}
