af1a4a5539eb5b562af734d333bad17b4620508c
junhyeong9812 <pickjog@gmail.com>
2026-03-02 18:27:55 +0900

    feat: LinkedDeque 초기 생성

--- practice/src/main/java/com/datastructure/practice0222/queue/LinkedDeque.java
+++ practice/src/main/java/com/datastructure/practice0222/queue/LinkedDeque.java
@@ -1,4 +1,26 @@
 package com.datastructure.practice0222.queue;
 
-public class LinkedDeque {
+/***
+ * 이중 연결 리스트 기반 덱 (Doubly Linked Deque)
+ *
+ * 양쪽 끝 삽입/삭제 모두 0(1), 용량 제한 없음.
+ */
+public class LinkedDeque<E> implements Deque<E> {
+
+    private static class Node<E> {
+        E value;
+        Node<E> prev;
+        Node<E> next;
+
+        Node(E value) {
+            this.value = value;
+        }
+    }
+
+    private Node<E> haed;
+    private Node<E> tail;
+    private int size;
+    
+    // Deque 연산
+
 }
