a22321c459facffd1354f3e798fc8cc0ec81410e
junhyeong9812 <pickjog@gmail.com>
2026-03-09 08:27:45 +0900

    feat: 연결 리스트 기반 시그니처 추가

---
+++ 04-queue-deque/src/main/java/com/datastructure/queue/pop/LinkedListDeque.java
@@ -0,0 +1,22 @@
+package com.datastructure.queue.pop;
+
+public class LinkedListDeque<E> {
+
+    public void addFirst(E element) {}
+
+    public void addLast(E element) {}
+
+    public E removeFirst() { return null; }
+
+    public E removeLast() { return null; }
+
+    public E peekFirst() { return null; }
+
+    public E peekLast() { return null; }
+
+    public int size() { return 0; }
+
+    public boolean isEmpty() { return true; }
+
+    public void clear() {}
+}
