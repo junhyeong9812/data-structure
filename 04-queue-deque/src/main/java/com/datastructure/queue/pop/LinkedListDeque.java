4cb747cf7432faa1e2593a80be5cca646723047d
junhyeong9812 <pickjog@gmail.com>
2026-03-10 08:49:15 +0900

    feat: LinkedListDeque 생성자

--- 04-queue-deque/src/main/java/com/datastructure/queue/pop/LinkedListDeque.java
+++ 04-queue-deque/src/main/java/com/datastructure/queue/pop/LinkedListDeque.java
@@ -2,6 +2,22 @@ package com.datastructure.queue.pop;
 
 public class LinkedListDeque<E> {
 
+    private Node<E> front;
+    private Node<E> rear;
+    private int size;
+
+    public static class Node<E> {
+        private E element;
+        private Node<E> prev;
+        private Node<E> next;
+
+        public Node(E element) { this.element = element; }
+
+        public void setPrev(Node<E> prev) { this.prev = prev; }
+
+        public void setNext(Node<E> next) { this.next = next; }
+    }
+
     public void addFirst(E element) {}
 
     public void addLast(E element) {}
