86defc4ad5b1a35d8b3aca59663edee59a006385
junhyeong9812 <pickjog@gmail.com>
2026-03-12 08:56:30 +0900

    feat: ArrayQueueImpl 초기 값 세팅

--- 04-queue-deque/src/main/java/com/datastructure/queue/oop/ArrayQueueImpl.java
+++ 04-queue-deque/src/main/java/com/datastructure/queue/oop/ArrayQueueImpl.java
@@ -1,4 +1,54 @@
 package com.datastructure.queue.oop;
 
 public class ArrayQueueImpl<E> implements Queue<E> {
+
+    private int capacity;
+    private E[] elements;
+    private int size;
+    private int front;
+    private int rear;
+
+    public ArrayQueueImpl() {
+        this.capacity = 10;
+        this.elements = (E[])(new Object[capacity]);
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    public ArrayQueueImpl(int capacity) {
+        this.capacity = capacity;
+        this.elements = (E[])(new Object[capacity]);
+        this.front = 0;
+        this.rear = 0;
+    }
+
+    @Override
+    public void enqueue(E element) {}
+
+    @Override
+    public boolean offer(E element) {
+        return true;
+    }
+    @Override
+    public E dequeue() {
+        return null;
+    }
+
+    @Override
+    public E poll() {return null;}
+
+    @Override
+    public E peek() {return null;}
+
+    @Override
+    public E front() {return null;}
+
+    @Override
+    public boolean isEmpty() {return true;}
+
+    @Override
+    public int size() {return 0;}
+
+    @Override
+    public void clear() {}
 }
