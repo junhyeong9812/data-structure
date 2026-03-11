7313c3d6359e198bae3423f50af3863068ab0475
junhyeong9812 <pickjog@gmail.com>
2026-03-12 07:56:08 +0900

    feat: Deque 인터페이스 추가

--- 04-queue-deque/src/main/java/com/datastructure/queue/oop/Deque.java
+++ 04-queue-deque/src/main/java/com/datastructure/queue/oop/Deque.java
@@ -1,4 +1,16 @@
 package com.datastructure.queue.oop;
 
 public interface Deque<E> extends Queue<E> {
+
+    void addFirst(E element);
+
+    void addLast(E element);
+
+    E removeFirst();
+
+    E removeLast();
+
+    E peekFirst();
+
+    E peekLast();
 }
