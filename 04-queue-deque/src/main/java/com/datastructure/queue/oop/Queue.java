befdb4e0f02b5a90ab3d6c73b59a21af25df9037
junhyeong9812 <pickjog@gmail.com>
2026-03-12 07:55:59 +0900

    feat: Queue 인터페이스 추가

--- 04-queue-deque/src/main/java/com/datastructure/queue/oop/Queue.java
+++ 04-queue-deque/src/main/java/com/datastructure/queue/oop/Queue.java
@@ -1,4 +1,22 @@
 package com.datastructure.queue.oop;
 
 public interface Queue<E> {
+
+    void enqueue(E element);
+
+    boolean offer(E element);
+
+    E dequeue();
+
+    E poll();
+
+    E peek();
+
+    E front();
+
+    boolean isEmpty();
+
+    int size();
+
+    void clear();
 }
