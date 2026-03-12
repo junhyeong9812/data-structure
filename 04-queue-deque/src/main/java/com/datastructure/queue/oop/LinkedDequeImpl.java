779bb639dd48f19f754c7ffb8370597b7ae5402c
junhyeong9812 <pickjog@gmail.com>
2026-03-13 08:04:18 +0900

    feat: LinkedDequeImpl 생성자 추가

--- 04-queue-deque/src/main/java/com/datastructure/queue/oop/LinkedDequeImpl.java
+++ 04-queue-deque/src/main/java/com/datastructure/queue/oop/LinkedDequeImpl.java
@@ -1,4 +1,29 @@
 package com.datastructure.queue.oop;
 
 public class LinkedDequeImpl<E> implements Deque<E> {
+
+    private Node<E> front;
+    private Node<E> rear;
+    private int size;
+
+    public LinkedDequeImpl() {
+        this.front = null;
+        this.rear = null;
+        this.size = 0;
+    }
+
+    private static class Node<E> {
+        private E element;
+        private Node<E> prev;
+        private Node<E> next;
+
+        public Node(E element) {
+            this.element = element;
+            this.prev = null;
+            this.next = null;
+        }
+        public void setPrev(Node<E> prev) {this.prev = prev; }
+        public void setNext(Node<E> next) {this.next = next; }
+    }
+
 }
