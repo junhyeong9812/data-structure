61a6940eceb44f20d10c726bb81668ebc39df451
junhyeong9812 <pickjog@gmail.com>
2026-03-14 22:15:04 +0900

    feat: SlidingWindowMax 로직 완료 큐 종료

--- 04-queue-deque/src/main/java/com/datastructure/queue/oop/SlidingWindowMax.java
+++ 04-queue-deque/src/main/java/com/datastructure/queue/oop/SlidingWindowMax.java
@@ -1,4 +1,24 @@
 package com.datastructure.queue.oop;
 
 public class SlidingWindowMax {
+    public int[] calculate(int[] nums, int windowsLength) {
+        if (nums.length == 0) {
+            throw new IllegalArgumentException();
+        }
+        if (nums.length < windowsLength) {
+            throw new IllegalArgumentException();
+        }
+        int count = nums.length - windowsLength + 1;
+        int[] result = new int[count];
+        for (int i = 0; i < count; i++) {
+            int max = nums[i];
+            for (int j = 0; j < windowsLength; j++) {
+                if (nums[i+j] > max) {
+                    max = nums[i+j];
+                }
+            }
+            result[i] = max;
+        }
+        return result;
+    }
 }
