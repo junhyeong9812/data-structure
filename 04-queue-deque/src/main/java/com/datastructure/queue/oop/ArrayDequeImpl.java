b61530824c899aa80074dba0587bc47bb47e70a5
junhyeong9812 <pickjog@gmail.com>
2026-03-14 10:38:59 +0900

    feat: ArrayDequeImpl 초기 생성자 추가

--- 04-queue-deque/src/main/java/com/datastructure/queue/oop/ArrayDequeImpl.java
+++ 04-queue-deque/src/main/java/com/datastructure/queue/oop/ArrayDequeImpl.java
@@ -1,4 +1,77 @@
 package com.datastructure.queue.oop;
 
 public class ArrayDequeImpl<E> implements Deque<E> {
+
+    private int capacity;
+    private E[] elements;
+    private int front;
+    private int rear;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public ArrayDequeImpl() {
+        this.capacity = 10;
+        this.elements = (E[])(new Object[capacity]);
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public ArrayDequeImpl(int selectCapacity) {
+        this.capacity = selectCapacity;
+        this.elements = (E[])(new Object[capacity]);
+        this.front = 0;
+        this.rear = 0;
+        this.size = 0;
+    }
+
+    @Override
+    public void enqueue(E element)  {
+        growArrayDeque();
+    }
+
+    @Override
+    public void addFirst(E element) {}
+
+    @Override
+    public void addLast(E element) {}
+
+    @Override
+    public boolean offer(E element) {return true;}
+
+    @Override
+    public E dequeue()  {return null;}
+
+    @Override
+    public E removeFirst() {return null;}
+
+    @Override
+    public E removeLast() {return null;}
+
+    @Override
+    public E poll() {return null;}
+
+    @Override
+    public E peek() {return null;}
+
+    @Override
+    public E peekFirst() {return null;}
+
+    @Override
+    public E peekLast() {return null;}
+
+    @Override
+    public E front() {return null;}
+
+    @Override
+    public boolean isEmpty() {return true;}
+
+    @Override
+    public int size() {return 0;}
+
+    @Override
+    public void clear() {}
+
+    
 }
