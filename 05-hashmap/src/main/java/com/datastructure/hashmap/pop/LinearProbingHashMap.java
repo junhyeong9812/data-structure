774ea9dbc8a02c047af6718956d443850e3be9c3
junhyeong9812 <pickjog@gmail.com>
2026-03-16 07:45:53 +0900

    feat: LinearProbingHashMap 메서드 시그니처 추가

--- 05-hashmap/src/main/java/com/datastructure/hashmap/pop/LinearProbingHashMap.java
+++ 05-hashmap/src/main/java/com/datastructure/hashmap/pop/LinearProbingHashMap.java
@@ -1,4 +1,12 @@
 package com.datastructure.hashmap.pop;
 
-public class LinearProbingHashMap {
+public class LinearProbingHashMap<K, V> {
+    public V put(K key, V value) {return null;}
+    public V get(K key) {return null;}
+    public V remove(K key) {return null;}
+    public boolean containsKey(K key) {return false;}
+    public boolean containsValue(V value) {return false;}
+    public int size() {return 0;}
+    public boolean isEmpty() {return true;}
+    public void clear() {}
 }
