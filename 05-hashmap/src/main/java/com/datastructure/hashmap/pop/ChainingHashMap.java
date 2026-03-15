fda721e1efc196e1b587bc87a94544c04a1c2e82
junhyeong9812 <pickjog@gmail.com>
2026-03-16 07:45:41 +0900

    feat: ChainingHashMap 메서드 시그니처 추가

--- 05-hashmap/src/main/java/com/datastructure/hashmap/pop/ChainingHashMap.java
+++ 05-hashmap/src/main/java/com/datastructure/hashmap/pop/ChainingHashMap.java
@@ -1,4 +1,13 @@
 package com.datastructure.hashmap.pop;
 
-public class ChainingHashMap {
+public class ChainingHashMap<K, V> {
+
+    public V put(K key, V value) {return null;}
+    public V get(K key) {return null;}
+    public V remove(K key) {return null;}
+    public boolean containsKey(K key) {return false;}
+    public boolean containsValue(V value) {return false;}
+    public int size() {return 0;}
+    public boolean isEmpty() {return true;}
+    public void clear() {}
 }
