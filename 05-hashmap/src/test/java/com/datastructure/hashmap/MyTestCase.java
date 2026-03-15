c9c2afcd5cfe2831c0bd4226e5913f671e92122e
junhyeong9812 <pickjog@gmail.com>
2026-03-16 08:12:36 +0900

    test: LinearProbingHashMap Test 시그니처 추가

--- 05-hashmap/src/test/java/com/datastructure/hashmap/MyTestCase.java
+++ 05-hashmap/src/test/java/com/datastructure/hashmap/MyTestCase.java
@@ -57,6 +57,51 @@ public class MyTestCase {
 
     @Nested
     @DisplayName("LinearProbingHashMap 테스트")
-    class LinearProbingHashMapTest {}
+    class LinearProbingHashMapTest {
+
+        @Nested
+        @DisplayName("put 메서드 테스트")
+        class PutTest {}
+
+        @Nested
+        @DisplayName("get 메서드 테스트")
+        class GetTest {}
+
+        @Nested
+        @DisplayName("remove 메서드 테스트")
+        class RemoveTest {}
+
+        @Nested
+        @DisplayName("containsKey 메서드 테스트")
+        class ContainsKeyTest {}
+
+        @Nested
+        @DisplayName("containValue 메서드 테스트")
+        class ContainsValueTest {}
+
+        @Nested
+        @DisplayName("size 메서드 테스트")
+        class SizeTest {}
+
+        @Nested
+        @DisplayName("isEmpty 메서드 테스트")
+        class IsEmptyTest {}
+
+        @Nested
+        @DisplayName("clear 메서드 테스트")
+        class ClearTest {}
+
+        @Nested
+        @DisplayName("keySet 메서드 테스트")
+        class KeySetTest {}
+
+        @Nested
+        @DisplayName("values 메서드 테스트")
+        class ValuesTest {}
+
+        @Nested
+        @DisplayName("entrySet 메서드 테스트")
+        class EntrySetTest {}
+    }
 
 }
