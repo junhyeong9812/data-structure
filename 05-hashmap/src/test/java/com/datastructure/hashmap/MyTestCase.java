c7593eb92534e6f5ef4844a913cebbac05a6e3d7
junhyeong9812 <pickjog@gmail.com>
2026-03-16 07:41:16 +0900

    test: hashmap 테스트 케이스 시그니처 생성

---
+++ 05-hashmap/src/test/java/com/datastructure/hashmap/MyTestCase.java
@@ -0,0 +1,16 @@
+package com.datastructure.hashmap;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+
+public class MyTestCase {
+
+    @Nested
+    @DisplayName("ChainingHashMap 테스트")
+    class ChainingHashMapTest {}
+
+    @Nested
+    @DisplayName("LinearProbingHashMap 테스트")
+    class LinearProbingHashMapTest {}
+
+}
