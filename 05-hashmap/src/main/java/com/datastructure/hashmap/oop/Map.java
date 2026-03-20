759e7ee05fc4c84768c6391b03ca671dc606e68a
junhyeong9812 <pickjog@gmail.com>
2026-03-21 08:48:51 +0900

    feat: Map인터페이스 추가

--- 05-hashmap/src/main/java/com/datastructure/hashmap/oop/Map.java
+++ 05-hashmap/src/main/java/com/datastructure/hashmap/oop/Map.java
@@ -1,4 +1,18 @@
 package com.datastructure.hashmap.oop;
 
+import java.util.Collection;
+import java.util.Set;
+
 public interface Map<K, V> {
+
+    V put(K key, V value);
+    V get(K key);
+    V remove(K key);
+    boolean containsKey(K key);
+    boolean containsValue(V value);
+    int size();
+    boolean isEmpty();
+    void clear();
+    Set<K> keySet();
+    Collection<V> values();
 }
