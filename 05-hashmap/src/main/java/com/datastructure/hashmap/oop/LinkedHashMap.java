6eec96913b13af97aaa8ee2d1b0342f88105ca60
junhyeong9812 <pickjog@gmail.com>
2026-03-21 13:42:57 +0900

    feat: LinkedHashMap 초기 세팅 추가

--- 05-hashmap/src/main/java/com/datastructure/hashmap/oop/LinkedHashMap.java
+++ 05-hashmap/src/main/java/com/datastructure/hashmap/oop/LinkedHashMap.java
@@ -1,4 +1,71 @@
 package com.datastructure.hashmap.oop;
 
-public class LinkedHashMap<K, V> extends HashMap<K, V> {
+import java.util.Collection;
+import java.util.Set;
+
+public class LinkedHashMap<K, V> implements Map<K, V> {
+
+    public static class Entry<K, V> {
+        K key;
+        V value;
+        Entry<K, V> next;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+            this.next = null;
+        }
+
+        public K getKey() { return key; }
+
+        public V getValue() { return value; }
+
+        public Entry<K , V> getNext() {return next;}
+
+        public void setNext(Entry<K, V> entry) {this.next = entry;}
+    }
+
+    private Entry<K, V>[] buckets;
+    private int capacity;
+    private int size;
+    private static final float LOAD_FACTOR = 0.75f;
+
+    @SuppressWarnings("unchecked")
+    public LinkedHashMap() {
+        this.capacity = 16;
+        this.buckets = new Entry[capacity];
+        this.size = 0;
+    }
+
+    @Override
+    public V put(K key, V value) { return null; }
+
+    @Override
+    public V get(K key) { return null; }
+
+    @Override
+    public V remove(K key) { return null; }
+
+    @Override
+    public boolean containsKey(K key) { return false; }
+
+    @Override
+    public boolean containsValue(V value) { return false; }
+
+    @Override
+    public int size() { return 0; }
+
+    @Override
+    public boolean isEmpty() { return true; }
+
+    @Override
+    public void clear() {}
+
+    @Override
+    public Set<K> keySet() { return null; }
+
+    @Override
+    public Collection<V> values() { return null; }
+
+    public Set<Entry<K, V>> entrySet() { return null; }
 }
