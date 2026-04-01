1b9c94886033cfc8b7fe906c57a6f605a49d5494
junhyeong9812 <pickjog@gmail.com>
2026-04-02 08:39:40 +0900

    feat: BST 인터페이스 시그니처 추가

--- 06-binary-search-tree/src/main/java/com/datastructure/bst/oop/BST.java
+++ 06-binary-search-tree/src/main/java/com/datastructure/bst/oop/BST.java
@@ -1,4 +1,27 @@
 package com.datastructure.bst.oop;
 
-public interface BST<T extends Comparable<T>> {
+import java.util.List;
+
+public interface BST<T extends Comparable<T>> extends Iterable<T> {
+
+    void insert(T value);
+    boolean search(T value);
+    void delete(T value);
+    boolean contains(T value);
+    T min();
+    T max();
+    int size();
+    boolean isEmpty();
+    void clear();
+    int height();
+    List<T> inorder();
+    List<T> preorder();
+    List<T> postorder();
+    List<T> levelorder();
+    T floor(T value);
+    T ceiling(T value);
+    int rank(T value);
+    T select(int k);
+    T predecessor(T value);
+    T successor(T value);
 }
