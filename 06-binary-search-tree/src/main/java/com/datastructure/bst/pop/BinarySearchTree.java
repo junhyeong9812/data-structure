e38e6c745701e31194a7c6cd35cab6853e055447
junhyeong9812 <pickjog@gmail.com>
2026-04-02 08:05:28 +0900

    feat: predecessor 구현

--- 06-binary-search-tree/src/main/java/com/datastructure/bst/pop/BinarySearchTree.java
+++ 06-binary-search-tree/src/main/java/com/datastructure/bst/pop/BinarySearchTree.java
@@ -146,8 +146,14 @@ public class BinarySearchTree<T extends Comparable<T>> {
         return values.get(k);
     }
 
+    public T predecessor(T value) {
+        List<T> values = inorder();
+        int index = values.indexOf(value);
+        if (index == -1) throw new IllegalArgumentException("트리에 존재하지 않는 값입니다.");
+        if (index == 0) return null;
+        return values.get(index - 1);
+    }
 
-    public T predecessor(T value) { return null; }
     public T successor(T value) { return null; }
 
     private TreeNode<T> insertNode(TreeNode<T> node, T value) {
