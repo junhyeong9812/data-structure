b940ed1f5e0f487cbe8801e4cfaeec7d09367e84
junhyeong9812 <pickjog@gmail.com>
2026-04-10 08:01:13 +0900

    feat: MinHeap 메서드 시그니처 제너릭 수정

--- 07-heap/src/main/java/com/datastructure/heap/pop/MinHeap.java
+++ 07-heap/src/main/java/com/datastructure/heap/pop/MinHeap.java
@@ -1,28 +1,28 @@
 package com.datastructure.heap.pop;
 
-public class MinHeap {
+public class MinHeap<E extends Comparable<E>> {
 
     public MinHeap() {}
     public MinHeap(int initialCapacity) {}
 
-    public void insert(int value) {}
-    public boolean offer(int value) { return false; }
+    public void insert(E value) {}
+    public boolean offer(E value) { return false; }
 
-    public int extractMin() { return 0; }
-    public Integer poll() { return null; }
+    public E extractMin() { return null; }
+    public E poll() { return null; }
 
-    public int getMin() { return 0; }
-    public Integer peek() { return null; }
+    public E getMin() { return null; }
+    public E peek() { return null; }
 
     public int size() { return 0; }
     public boolean isEmpty() { return false; }
     public void clear() {}
 
-    public static MinHeap heapify(int[] array) { return null; }
+    public static <E extends Comparable<E>> MinHeap<E> heapify(E[] array) { return null; }
 
-    public void decreaseKey(int index, int newValue) {}
+    public void decreaseKey(int index, E newValue) {}
 
-    public int delete(int index) { return 0; }
+    public E delete(int index) { return null; }
 
-    public MinHeap merge(MinHeap other) { return null; }
+    public MinHeap<E> merge(MinHeap<E> other) { return null; }
 }
