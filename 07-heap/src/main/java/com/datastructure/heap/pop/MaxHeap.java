f75c467989695007d072fea74720e34d6a85ed35
junhyeong9812 <pickjog@gmail.com>
2026-04-10 07:54:50 +0900

    feat: MaxHeap 메서드 시그니처 추가

--- 07-heap/src/main/java/com/datastructure/heap/pop/MaxHeap.java
+++ 07-heap/src/main/java/com/datastructure/heap/pop/MaxHeap.java
@@ -1,4 +1,28 @@
 package com.datastructure.heap.pop;
 
 public class MaxHeap {
+
+    public MaxHeap() {}
+    public MaxHeap(int initialCapacity) {}
+
+    public void insert(int value) {}
+    public boolean offer(int value) { return false; }
+
+    public int extractMax() { return 0; }
+    public Integer poll() { return null; }
+
+    public int getMax() { return 0; }
+    public Integer peek() { return null; }
+
+    public int size() { return 0; }
+    public boolean isEmpty() { return false; }
+    public void clear() {}
+
+    public static MaxHeap heapify(int[] array) { return null; }
+
+    public void increaseKey(int index, int newValue) {}
+
+    public int delete(int index) { return 0; }
+
+    public MaxHeap merge(MaxHeap other) { return null; }
 }
