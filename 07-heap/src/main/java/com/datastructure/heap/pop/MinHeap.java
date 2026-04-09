9fc55cca646f0a93992abf81d50d23e058a98f61
junhyeong9812 <pickjog@gmail.com>
2026-04-10 07:55:01 +0900

    feat: MinHeap 메서드 시그니처 추가

--- 07-heap/src/main/java/com/datastructure/heap/pop/MinHeap.java
+++ 07-heap/src/main/java/com/datastructure/heap/pop/MinHeap.java
@@ -1,4 +1,28 @@
 package com.datastructure.heap.pop;
 
 public class MinHeap {
+
+    public MinHeap() {}
+    public MinHeap(int initialCapacity) {}
+
+    public void insert(int value) {}
+    public boolean offer(int value) { return false; }
+
+    public int extractMin() { return 0; }
+    public Integer poll() { return null; }
+
+    public int getMin() { return 0; }
+    public Integer peek() { return null; }
+
+    public int size() { return 0; }
+    public boolean isEmpty() { return false; }
+    public void clear() {}
+
+    public static MinHeap heapify(int[] array) { return null; }
+
+    public void decreaseKey(int index, int newValue) {}
+
+    public int delete(int index) { return 0; }
+
+    public MinHeap merge(MinHeap other) { return null; }
 }
