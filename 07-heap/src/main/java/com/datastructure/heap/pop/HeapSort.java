9e867494c131f273bd799c04730ebd8a12a4ec79
junhyeong9812 <pickjog@gmail.com>
2026-04-20 08:04:40 +0900

    feat: HeapSort 시그니처 추가

--- 07-heap/src/main/java/com/datastructure/heap/pop/HeapSort.java
+++ 07-heap/src/main/java/com/datastructure/heap/pop/HeapSort.java
@@ -1,4 +1,8 @@
 package com.datastructure.heap.pop;
 
 public class HeapSort {
+
+    public static int[] sortAscending(int[] array) { return null; }
+
+    public static int[] sortDescending(int[] array) { return null; }
 }
