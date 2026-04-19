e110b79412385d0b8798784fec1a7cd292cdbf35
junhyeong9812 <pickjog@gmail.com>
2026-04-20 08:04:51 +0900

    feat: HeapProblems 시그니처 추가

--- 07-heap/src/main/java/com/datastructure/heap/pop/HeapProblems.java
+++ 07-heap/src/main/java/com/datastructure/heap/pop/HeapProblems.java
@@ -1,4 +1,18 @@
 package com.datastructure.heap.pop;
 
+import java.util.List;
+
 public class HeapProblems {
+
+    // Top-K: 배열에서 가장 큰 k개 요소 반환
+    public static List<Integer> topK(int[] array, int k) { return null; }
+
+    // K번째 큰 요소: 배열에서 k번째로 큰 값 반환
+    public static int kthLargest(int[] array, int k) { return 0; }
+
+    // 중앙값 스트림: 숫자가 하나씩 들어올 때마다 현재까지의 중앙값 반환
+    public static double[] medianStream(int[] stream) { return null; }
+
+    // 다익스트라: 가중치 그래프에서 시작점으로부터 각 노드까지 최단거리
+    public static int[] dijkstra(int[][] graph, int start) { return null; }
 }
