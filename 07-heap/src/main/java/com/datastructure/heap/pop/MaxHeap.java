6c17aadb3c85f1b9963f5a8098200248d032d490
junhyeong9812 <pickjog@gmail.com>
2026-04-27 08:49:53 +0900

    feat: add growCapacity/collapseCapacity

--- 07-heap/src/main/java/com/datastructure/heap/pop/MaxHeap.java
+++ 07-heap/src/main/java/com/datastructure/heap/pop/MaxHeap.java
@@ -1,5 +1,7 @@
 package com.datastructure.heap.pop;
 
+import java.util.Arrays;
+
 public class MaxHeap<E extends Comparable<E>> {
 
     private E[] array;
@@ -15,7 +17,22 @@ public class MaxHeap<E extends Comparable<E>> {
         this.size = 0;
     }
 
-    public void insert(E value) {}
+    public void insert(E value) {
+        array[size] = value;
+        for (int i = 0; i < array.length; i++) {
+            int cmp = value.compareTo(array[i]);
+            if (cmp == 0) {
+                System.arraycopy(array, i, array, i+1,array.length);
+                array[i] = value;
+                break;
+            }
+            if (cmp > 0) {
+                System.arraycopy(array, i, array, i+1,array.length);
+                array[i] = value;
+                break;
+            }
+        }
+    }
     public boolean offer(E value) { return false; }
 
     public E extractMax() { return null; }
@@ -35,4 +52,18 @@ public class MaxHeap<E extends Comparable<E>> {
     public E delete(int index) { return null; }
 
     public MaxHeap<E> merge(MaxHeap<E> other) { return null; }
+
+    private void growCapacity() {
+        int capacity = this.array.length;
+        if (this.size >= capacity) {
+            array = Arrays.copyOf(array, (int)(capacity * 1.5));
+        }
+    }
+
+    private void collapseCapacity() {
+        int capacity = this.array.length;
+        if (this.size <= capacity/4 && capacity> 10) {
+            array = Arrays.copyOf(array,Math.max(capacity/2, 10));
+        }
+    }
 }
