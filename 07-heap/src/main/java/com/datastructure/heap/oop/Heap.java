b749240c01900e6174cce4eec936b7bbe33b7b11
junhyeong9812 <pickjog@gmail.com>
2026-05-11 08:43:33 +0900

    feat: add Heap interface in oop

--- 07-heap/src/main/java/com/datastructure/heap/oop/Heap.java
+++ 07-heap/src/main/java/com/datastructure/heap/oop/Heap.java
@@ -1,4 +1,10 @@
 package com.datastructure.heap.oop;
 
 public interface Heap<E> {
+    void insert(E value);
+    E extract();
+    E peek();
+    int size();
+    boolean isEmpty();
+    void clear();
 }
