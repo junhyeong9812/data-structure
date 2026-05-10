4ea275c54895627ab808132b857090c3599f15ef
junhyeong9812 <pickjog@gmail.com>
2026-05-11 08:43:23 +0900

    feat: add BinaryHeap in insert method

--- 07-heap/src/main/java/com/datastructure/heap/oop/BinaryHeap.java
+++ 07-heap/src/main/java/com/datastructure/heap/oop/BinaryHeap.java
@@ -1,4 +1,55 @@
 package com.datastructure.heap.oop;
 
+
+import java.util.Arrays;
+import java.util.Comparator;
+
 public class BinaryHeap<E> implements Heap<E>, PriorityQueue<E> {
+    private E[] array;
+    private int size;
+    private final Comparator<? super E> comparator;
+
+    @SuppressWarnings("unchecked")
+    public BinaryHeap(Comparator<? super E> comparator) {
+        this.array = (E[]) new Object[10];
+        this.size = 0;
+        this.comparator = comparator;
+    }
+
+    public static <E extends Comparable<E>> BinaryHeap<E> maxHeap() {
+        return new BinaryHeap<>(Comparator.reverseOrder());
+    }
+
+    public static <E extends Comparable<E>> BinaryHeap<E> minHeap() {
+        return new BinaryHeap<>(Comparator.naturalOrder());
+    }
+
+    @Override
+    public void insert(E value) {
+        if (value == null) throw new IllegalArgumentException();
+        growCapacity();
+        array[size] = value;
+        siftUp(size);
+        size++;
+    }
+
+    private void siftUp(int index) {
+        while (index > 0) {
+            int parent = (index - 1) / 2;
+            if (comparator.compare(array[index], array[parent]) < 0) {
+                E tmp = array[index];
+                array[index] = array[parent];
+                array[parent] = tmp;
+                index = parent;
+            } else {
+                break;
+            }
+        }
+    }
+
+    private void growCapacity() {
+        if (size >= array.length) {
+            array = Arrays.copyOf(array, (int)(array.length * 1.5));
+        }
+    }
 }
