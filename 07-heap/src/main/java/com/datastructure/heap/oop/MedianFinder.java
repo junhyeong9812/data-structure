3fab70775ba992dd95a914578cef4484e8fd3bba
junhyeong9812 <pickjog@gmail.com>
2026-05-12 08:47:33 +0900

    feat: add MedianFinder

--- 07-heap/src/main/java/com/datastructure/heap/oop/MedianFinder.java
+++ 07-heap/src/main/java/com/datastructure/heap/oop/MedianFinder.java
@@ -1,4 +1,34 @@
 package com.datastructure.heap.oop;
 
 public class MedianFinder {
+    private final BinaryHeap<Integer> lower;
+    private final BinaryHeap<Integer> upper;
+
+    public MedianFinder() {
+        this.lower = BinaryHeap.maxHeap();
+        this.upper = BinaryHeap.minHeap();
+    }
+
+    public void addNum(int num) {
+        if (lower.isEmpty() || num <= lower.peek()) {
+            lower.insert(num);
+        } else {
+            upper.insert(num);
+        }
+
+        //균형 유지
+        if (lower.size() > upper.size() + 1) {
+            upper.insert(lower.extract());
+        } else if (upper.size() > lower.size()) {
+            lower.insert(upper.extract());
+        }
+    }
+
+    public double findMedian() {
+        if (lower.isEmpty()) throw new IllegalArgumentException("데이터가 없습니다.");
+        if (lower.size() > upper.size()) {
+            return lower.peek();
+        }
+        return (lower.peek() + upper.peek()) / 2.0;
+    }
 }
