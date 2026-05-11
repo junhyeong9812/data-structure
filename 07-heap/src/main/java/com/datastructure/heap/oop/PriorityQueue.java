025d99aa7a62121da58bdd7cb949dbeff7c0618a
junhyeong9812 <pickjog@gmail.com>
2026-05-12 08:22:27 +0900

    feat: add peek signature

--- 07-heap/src/main/java/com/datastructure/heap/oop/PriorityQueue.java
+++ 07-heap/src/main/java/com/datastructure/heap/oop/PriorityQueue.java
@@ -3,7 +3,7 @@ package com.datastructure.heap.oop;
 public interface PriorityQueue<E> {
     boolean offer(E value);
     E poll();
-    E peak();
+    E peek();
     int size();
     boolean isEmpty();
 }
