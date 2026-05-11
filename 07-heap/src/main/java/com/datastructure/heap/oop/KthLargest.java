2822c969ebddba35a47aff950c34159237d9f173
junhyeong9812 <pickjog@gmail.com>
2026-05-12 08:47:47 +0900

    feat: add KthLargest

--- 07-heap/src/main/java/com/datastructure/heap/oop/KthLargest.java
+++ 07-heap/src/main/java/com/datastructure/heap/oop/KthLargest.java
@@ -1,4 +1,24 @@
 package com.datastructure.heap.oop;
 
 public class KthLargest {
+    private final int k;
+    private final BinaryHeap<Integer> heap;
+
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        this.heap = BinaryHeap.minHeap();
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+        if (heap.size() < k) {
+            heap.insert(val);
+        } else if (val > heap.peek()) {
+            heap.extract();
+            heap.insert(val);
+        }
+        return heap.peek();
+    }
 }
