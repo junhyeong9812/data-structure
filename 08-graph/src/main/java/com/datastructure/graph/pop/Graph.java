98feb5c0d9a6da4ac1555a25909d53df7add0fc1
junhyeong9812 <pickjog@gmail.com>
2026-05-18 08:08:34 +0900

    feat(graph): setup signature Method

--- 08-graph/src/main/java/com/datastructure/graph/pop/Graph.java
+++ 08-graph/src/main/java/com/datastructure/graph/pop/Graph.java
@@ -1,4 +1,26 @@
 package com.datastructure.graph.pop;
 
+import java.util.*;
+
 public class Graph {
+
+    public void addVertex(int v) {}
+
+    public void addEdge(int u, int v) {}
+
+    public void removeVertex(int v) {}
+
+    public void removeEdge(int u, int v) {}
+
+    public boolean hasVertex(int v) { return false; }
+
+    public boolean hasEdge(int u, int v) { return false; }
+
+    public List<Integer> getNeighbors(int v) { return null; }
+
+    public int vertexCount() { return 0; }
+
+    public int edgeCount() { return 0; }
+
+    public boolean isEmpty() { return false; }
 }
