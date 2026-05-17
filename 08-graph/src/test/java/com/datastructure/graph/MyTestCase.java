b723755980cc383590c36ac8ad616ccfa760e19b
junhyeong9812 <pickjog@gmail.com>
2026-05-18 08:06:53 +0900

    test: setup signature Test

--- 08-graph/src/test/java/com/datastructure/graph/MyTestCase.java
+++ 08-graph/src/test/java/com/datastructure/graph/MyTestCase.java
@@ -5,53 +5,123 @@ import org.junit.jupiter.api.Nested;
 
 public class MyTestCase {
 
-    @Nested @DisplayName("인접 리스트 그래프 (무방향)")
-    class AdjacencyListGraphTest {
+    // ===== 기본 그래프 구현체 (CRUD) =====
 
+    @Nested @DisplayName("Graph (무방향, 인접 리스트) 테스트")
+    class GraphTest {
+        @Nested @DisplayName("addVertex 테스트")
+        class AddVertexTest {}
+
+        @Nested @DisplayName("addEdge 테스트")
+        class AddEdgeTest {}
+
+        @Nested @DisplayName("removeVertex 테스트")
+        class RemoveVertexTest {}
+
+        @Nested @DisplayName("removeEdge 테스트")
+        class RemoveEdgeTest {}
+
+        @Nested @DisplayName("hasVertex 테스트")
+        class HasVertexTest {}
+
+        @Nested @DisplayName("hasEdge 테스트")
+        class HasEdgeTest {}
+
+        @Nested @DisplayName("getNeighbors 테스트")
+        class GetNeighborsTest {}
+
+        @Nested @DisplayName("vertexCount / edgeCount 테스트")
+        class CountTest {}
     }
 
-    @Nested @DisplayName("인접 리스트 그래프 (방향)")
-    class DirectedAdjacencyListGraphTest {}
+    @Nested @DisplayName("DirectedGraph (방향, 인접 리스트) 테스트")
+    class DirectedGraphTest {
+        @Nested @DisplayName("addEdge 테스트")
+        class AddEdgeTest {}
 
-    @Nested @DisplayName("인접 행렬 그래프 (무방향)")
-    class AdjacencyMatrixGraphTest {}
+        @Nested @DisplayName("removeEdge 테스트")
+        class RemoveEdgeTest {}
 
-    @Nested @DisplayName("인접 행렬 그래프 (방향)")
-    class DirectedAdjacencyMatrixGraphTest {}
+        @Nested @DisplayName("hasEdge 테스트")
+        class HasEdgeTest {}
 
-    @Nested @DisplayName("BFS 테스트")
-    class BfsTest {}
+        @Nested @DisplayName("getNeighbors 테스트")
+        class GetNeighborsTest {}
+    }
 
-    @Nested @DisplayName("DFS 테스트")
-    class DfsTest {}
+    @Nested @DisplayName("WeightedGraph (가중치, 인접 리스트) 테스트")
+    class WeightedGraphTest {
+        @Nested @DisplayName("addEdge 가중치 간선 테스트")
+        class AddEdgeTest {}
+    }
 
-    @Nested @DisplayName("최단 경로 (비가중치) 테스트")
-    class ShortestPathTest {}
+    // ===== 인접 행렬 구현체 (CRUD) =====
 
-    @Nested @DisplayName("다익스트라 테스트")
-    class DijkstraTest {}
+    @Nested @DisplayName("AdjacencyMatrixGraph (무방향, 인접 행렬) 테스트")
+    class AdjacencyMatrixGraphTest {
+        @Nested @DisplayName("addVertex 테스트")
+        class AddVertexTest {}
 
-    @Nested @DisplayName("벨만-포드 테스트")
-    class BellmanFordTest {}
+        @Nested @DisplayName("addEdge 테스트")
+        class AddEdgeTest {}
 
-    @Nested @DisplayName("플로이드-워셜 테스트")
-    class FloydWarshallTest {}
+        @Nested @DisplayName("removeVertex 테스트")
+        class RemoveVertexTest {}
 
-    @Nested @DisplayName("사이클 탐지 테스트")
-    class CycleDetectionTest {}
+        @Nested @DisplayName("removeEdge 테스트")
+        class RemoveEdgeTest {}
 
-    @Nested @DisplayName("위상 정렬 테스트")
-    class TopologicalSortTest {}
+        @Nested @DisplayName("hasEdge 테스트")
+        class HasEdgeTest {}
+    }
 
-    @Nested @DisplayName("연결 요소 테스트")
-    class ConnectedComponentsTest {}
+    @Nested @DisplayName("DirectedAdjacencyMatrixGraph (방향, 인접 행렬) 테스트")
+    class DirectedAdjacencyMatrixGraphTest {
+        @Nested @DisplayName("addEdge 테스트")
+        class AddEdgeTest {}
 
-    @Nested @DisplayName("이분 그래프 테스트")
-    class BipartiteTest {}
+        @Nested @DisplayName("hasEdge 테스트")
+        class HasEdgeTest {}
+    }
 
-    @Nested @DisplayName("프림 MST 테스트")
-    class PrimTest {}
+    // ===== 응용 알고리즘 =====
 
-    @Nested @DisplayName("크루스칼 MST 테스트")
-    class KruskalTest {}
+    @Nested @DisplayName("GraphProblems 테스트")
+    class GraphProblemsTest {
+        @Nested @DisplayName("BFS 테스트")
+        class BfsTest {}
+
+        @Nested @DisplayName("DFS 테스트")
+        class DfsTest {}
+
+        @Nested @DisplayName("최단 경로 (비가중치) 테스트")
+        class ShortestPathTest {}
+
+        @Nested @DisplayName("다익스트라 테스트")
+        class DijkstraTest {}
+
+        @Nested @DisplayName("벨만-포드 테스트")
+        class BellmanFordTest {}
+
+        @Nested @DisplayName("플로이드-워셜 테스트")
+        class FloydWarshallTest {}
+
+        @Nested @DisplayName("사이클 탐지 테스트")
+        class CycleDetectionTest {}
+
+        @Nested @DisplayName("위상 정렬 테스트")
+        class TopologicalSortTest {}
+
+        @Nested @DisplayName("연결 요소 테스트")
+        class ConnectedComponentsTest {}
+
+        @Nested @DisplayName("이분 그래프 테스트")
+        class BipartiteTest {}
+
+        @Nested @DisplayName("프림 MST 테스트")
+        class PrimTest {}
+
+        @Nested @DisplayName("크루스칼 MST 테스트")
+        class KruskalTest {}
+    }
 }
