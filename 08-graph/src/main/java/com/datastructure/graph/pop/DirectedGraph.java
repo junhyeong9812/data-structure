05c70627dbb30385606ab71ce5084a077b220b37
junhyeong9812 <pickjog@gmail.com>
2026-05-27 07:36:21 +0900

    feat(graph): DirectedGraph signature

--- 08-graph/src/main/java/com/datastructure/graph/pop/DirectedGraph.java
+++ 08-graph/src/main/java/com/datastructure/graph/pop/DirectedGraph.java
@@ -1,4 +1,32 @@
 package com.datastructure.graph.pop;
 
+import java.util.*;
+
 public class DirectedGraph {
+
+    private Map<Integer, List<Integer>> adjList;
+
+    public DirectedGraph() {
+        this.adjList = new HashMap<>();
+    }
+
+    public void addVertex(int v) {}
+
+    public void addEdge(int u, int v) {}
+
+    public void removeVertex(int v) {}
+
+    public void removeEdge(int u, int v) {}
+
+    public boolean hasVertex(int v) { return false; }
+
+    public boolean hasEdge(int u, int v) { return false; }
+
+    public List<Integer> getNeighbors(int v) { return null; }
+
+    public int vertexCount() { return 0; }
+
+    public int edgeCount() { return 0; }
+
+    public boolean isEmpty() { return false; }
 }
