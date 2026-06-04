5217c8587dc4859eff97a66ea8c3e4d81724a135
junhyeong9812 <pickjog@gmail.com>
2026-06-05 08:41:48 +0900

    feat(graph): add removeEdge in weightGraph

--- 08-graph/src/main/java/com/datastructure/graph/pop/WeightedGraph.java
+++ 08-graph/src/main/java/com/datastructure/graph/pop/WeightedGraph.java
@@ -32,7 +32,12 @@ public class WeightedGraph {
         }
     }
 
-    public void removeEdge(int u, int v) {}
+    public void removeEdge(int u, int v) {
+        if (!adjList.containsKey(u)) throw new IllegalArgumentException("존재하지 않는 정점입니다: " + u);
+        if (!adjList.containsKey(v)) throw new IllegalArgumentException("존재하지 않는 정점입니다: " + v);
+        if (!adjList.get(u).containsKey(v)) throw new IllegalArgumentException("존재하지 않는 간선입니다: " + u + "-" + v);
+        adjList.get(u).remove(v);
+    }
 
     public boolean basVertex(int v) {
         return false;
